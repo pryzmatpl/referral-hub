5a5dd9c4c05806786064b3c3c32fe61ebd541816
Piotr Słupski <piotroxp@gmail.com>
2024-04-04 18:03:19 +0200

    lint

--- refairmefrontend1/webpack.config.js
+++ refairmefrontend1/webpack.config.js
@@ -1,74 +1,75 @@
-'use strict'
-const path = require('path')
-const utils = require('./utils')
-const config = require('./config')
-const Dotenv = require('dotenv-webpack')
-const vueLoaderConfig = require('./build/vue-loader.conf')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+"use strict"
+const path = require("path")
+const utils = require("./utils")
+const config = require("./config")
+const Dotenv = require("dotenv-webpack")
+const vueLoaderConfig = require("./build/vue-loader.conf")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 function resolve(dir) {
-  return path.join(__dirname, '.', dir)
+  return path.join(__dirname, ".", dir)
 }
 
 module.exports = {
-  mode: 'production',
-  context: path.resolve(__dirname, './'),
+  mode: "production",
+  context: path.resolve(__dirname, "./"),
   plugins: [
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jquery: 'jquery',
-      'window.jQuery': 'jquery',
-      jQuery: 'jquery'
+      $: "jquery",
+      jquery: "jquery",
+      "window.jQuery": "jquery",
+      jQuery: "jquery",
     }),
     new Dotenv({
-      path: './config/.env', // Path to .env file (this is the default)
-      safe: false // load .env.example (defaults to "false" which does not use dotenv-safe)
+      path: "./config/.env", // Path to .env file (this is the default)
+      safe: false, // load .env.example (defaults to "false" which does not use dotenv-safe)
     }),
     new webpack.DefinePlugin({
-      'process.env': require('./config/dev.env')
+      "process.env": require("./config/dev.env"),
     }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true
+      filename: "index.html",
+      template: "index.html",
+      inject: true,
     }),
-    new BundleAnalyzerPlugin()    
+    new ExtractTextPlugin(),
+    new BundleAnalyzerPlugin(),    
   ],
   optimization: {
     splitChunks: {
-      chunks: 'all'
-    }
+      chunks: "all",
+    },
   },
   entry: {
-    app: './src/main.js'
+    app: "./src/main.js",
   },
   devServer: {
-    hot: true
+    hot: true,
   },
   output: {
     path: config.build.assetsRoot,
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
-    publicPath: process.env.NODE_ENV === 'production' ?
-      config.build.assetsPublicPath : config.dev.assetsPublicPath
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js",
+    publicPath: process.env.NODE_ENV === "production" ?
+      config.build.assetsPublicPath : config.dev.assetsPublicPath,
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: [".js", ".vue", ".json"],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
-    }
+      "vue$": "vue/dist/vue.esm.js",
+      "@": resolve("src"),
+    },
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
-        options: vueLoaderConfig
+        loader: "vue-loader",
+        options: vueLoaderConfig,
       },
       /*
       {
@@ -79,60 +80,60 @@ module.exports = {
       },*/
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        include: [resolve("src"), resolve("test"), resolve("node_modules/webpack-dev-server/client")],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
+          name: utils.assetsPath("img/[name].[hash:7].[ext]"),
+        },
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
-        }
+          name: utils.assetsPath("media/[name].[hash:7].[ext]"),
+        },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
+          name: utils.assetsPath("fonts/[name].[hash:7].[ext]"),
+        },
       },
       {
         test: /\.sass$/,
         use: [{
-          loader: "sass-loader"
-        }]
+          loader: "sass-loader",
+        }],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.html$/,
         use: [
           {
             loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
-      }
-    ]
+            options: {minimize: true},
+          },
+        ],
+      },
+    ],
   },
   node: {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
     // source contains it (although only uses it if it's native).
     __dirname: false,
     __filename: false,
-    global: true
-  }
+    global: true,
+  },
 }
