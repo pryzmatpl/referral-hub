da947cc65b8edb497808daeae88410917f9c2965
Piotr Słupski <piotroxp@gmail.com>
2024-10-27 22:22:22 +0100

    Temporary DB update

--- refairmebackend1/bootstrap/app.php
+++ refairmebackend1/bootstrap/app.php
@@ -25,13 +25,22 @@ $capsule = new Capsule;
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver' => getenv('DB_DRIVER'),
+/*    'driver' => getenv('DB_DRIVER'),
     'host' => getenv('DB_HOST'),
     'database' => getenv('DB_DATABASE'),
     'username' => getenv('DB_USERNAME'),
     'password' => getenv('DB_PASSWORD'),
     'charset' => 'utf8',
     'port' => getenv('DB_PORT'),
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => ''*/
+    'driver' => "mysql",
+    'host' => "mysql_aimatch",
+    'database' => "prism",
+    'username' => "root",
+    'password' => "1tJ3nBCxy",
+    'charset' => 'utf8',
+    'port' => "3306",
     'collation' => 'utf8_unicode_ci',
     'prefix' => ''
 ]);
