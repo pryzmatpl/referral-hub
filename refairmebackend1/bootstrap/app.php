12ed89f86a7bce74291895155d6dde54e31f9656
Piotr Słupski <piotroxp@gmail.com>
2024-12-16 21:24:58 +0100

    Order invalid

--- refairmebackend1/bootstrap/app.php
+++ refairmebackend1/bootstrap/app.php
@@ -22,28 +22,25 @@ use Monolog\Processor\IntrospectionProcessor;
 $container = new Container();
 $capsule = new Capsule;
 
-$capsule = new Capsule;
-
-$capsule->addConnection([
-    'driver' => getenv('DB_DRIVER'),
-    'host' => getenv('DB_HOST'),
-    'database' => getenv('DB_DATABASE'),
-    'username' => getenv('DB_USERNAME'),
-    'password' => getenv('DB_PASSWORD'),
-    'charset' => 'utf8',
-    'port' => getenv('DB_PORT'),
-    'collation' => 'utf8_unicode_ci',
-    'prefix' => ''
-]);
-
-$capsule->setAsGlobal();
-$capsule->bootEloquent();
-
-
 // Load environment variables
 try {
     $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
+
+    $capsule->addConnection([
+        'driver' => getenv('DB_DRIVER'),
+        'host' => getenv('DB_HOST'),
+        'database' => getenv('DB_DATABASE'),
+        'username' => getenv('DB_USERNAME'),
+        'password' => getenv('DB_PASSWORD'),
+        'charset' => 'utf8',
+        'port' => getenv('DB_PORT'),
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => ''
+    ]);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
 } catch (InvalidPathException $e) {
     error_log('Could not load .env file: ' . $e->getMessage());
 }
