f36c9f92740d4b6438bced3770c8fba06af59247
Razthyae <a_kumar@poczta.fm>
2025-02-12 10:37:20 +0100

    Changed userweight model to be compatible with table

--- refairmebackend1/app/Models/Userweight.php
+++ refairmebackend1/app/Models/Userweight.php
@@ -6,23 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 class Userweight extends Model
 {
 
-  protected $table = 'userweights';
+  protected $table = 'user_weights';
 
   protected $fillable = [
-			 'aone',
-			 'atwo',
-			 'athree',
-			 'afour',
-			 'afive',
-			 'asix',
-			 'aseven',
-			 'aeight',
-			 'anine',
-			 'aten',
-			 'aeleven',
-			 'userid',
-			 'keywords'
-			 ];
+    'weight_one',
+    'weight_two',
+    'weight_three',
+    'weight_four',
+    'weight_five',
+    'weight_six',
+    'weight_seven',
+    'weight_eight',
+    'weight_nine',
+    'weight_ten',
+    'weight_eleven',
+    'user_id',
+    'keywords'
+];
+
 
      protected $casts = [
         'keywords' => 'array'
