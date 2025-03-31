06a89b8aaf3eef46697d2291bfc7a56a4061d6f2
Piotr Słupski <piotroxp@gmail.com>
2025-03-31 22:02:45 +0200

    UI broken.

--- refrme-frontend/build/webpack.base.conf.js
+++ refrme-frontend/build/webpack.base.conf.js
@@ -1,96 +1,150 @@
-'use strict'
-const path = require('path')
-const utils = require('./utils')
-const config = require('../config')
+const path = require("path");
+const utils = require("./utils");
+const config = require("../config");
+const webpack = require("webpack");
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const vueLoaderConfig = require('./vue-loader.conf')
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 function resolve(dir) {
-  return path.join(__dirname, '..', dir)
+  return path.join(__dirname, "../", dir);
 }
 
 module.exports = {
-  context: path.resolve(__dirname, '../'),
+  mode: "production",
+  context: path.resolve(__dirname, "../"),
   entry: {
-    app: './src/main.js'
+    app: path.resolve(__dirname, "../src/main.js")
   },
   output: {
     path: config.build.assetsRoot,
-    filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production' ?
-      config.build.assetsPublicPath :
-      config.dev.assetsPublicPath
+    filename: utils.assetsPath("js/[name].[contenthash].js"),
+    chunkFilename: utils.assetsPath("js/[name].[contenthash].js"),
+    publicPath: process.env.NODE_ENV === "production"
+        ? config.build.assetsPublicPath
+        : config.dev.assetsPublicPath,
+    clean: true, // Replaces CleanWebpackPlugin
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
-     alias: {
-      'vue$': 'vue/dist/vue.esm-bundler.js',
-      '@': path.resolve(__dirname, '../src'),
-    }
+    extensions: [".js", ".vue", ".json"],
+    alias: {
+      "vue$": "vue/dist/vue.esm-bundler.js",
+      "@": resolve("src"),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ],
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          vueLoaderConfig,
-          extractCSS: true
-        }
+        loader: "vue-loader",
       },
       {
-        test: /\.js$/,        
-        include: [
-          resolve('src'),
-          resolve('test'),
-          resolve('node_modules/webpack-dev-server/client')
-        ]
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: [resolve("src"), resolve("test")],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024 // 10kb
+          }
+        },
+        generator: {
+          filename: utils.assetsPath("img/[name].[hash:7][ext]")
         }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024 // 10kb
+          }
+        },
+        generator: {
+          filename: utils.assetsPath("media/[name].[hash:7][ext]")
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024 // 10kb
+          }
+        },
+        generator: {
+          filename: utils.assetsPath("fonts/[name].[hash:7][ext]")
         }
       },
       {
-        test: /\.sass$/,
-        use: [{
-          loader: "sass-loader",
-        }]
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                indentedSyntax: false
+              }
+            }
+          }
+        ]
       },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
-      {
-        test: /\.html$/,
-        use: ['html-loader']
-      }
-    ]
+    ],
   },
   plugins: [
-    new MiniCssExtractPlugin()
+    new VueLoaderPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jquery: "jquery",
+      "window.jQuery": "jquery",
+      jQuery: "jquery",
+    }),
+    new Dotenv({
+      path: "../config/.env",
+      safe: false,
+    }),
+    new HtmlWebpackPlugin({
+      filename: "../static/index.html",
+      template: "../static/index.html",
+      inject: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: utils.assetsPath("css/[name].[contenthash].css"),
+      chunkFilename: utils.assetsPath("css/[id].[contenthash].css"),
+    }),
+    new BundleAnalyzerPlugin(),
   ],
-  node: {
-    __dirname: false,
-    __filename: false,
-    global: true
-  }
-}
+  devServer: {
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
--- refrme-frontend/build/webpack.dev.conf.js
+++ refrme-frontend/build/webpack.dev.conf.js
@@ -1,34 +1,33 @@
 'use strict'
-const utils = require('./utils')
-const webpack = require('webpack')
-const config = require('../config')
-const { merge } = require('webpack-merge')
-const path = require('path')
-const baseWebpackConfig = require('./webpack.base.conf')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const portfinder = require('portfinder')
-const { VueLoaderPlugin } = require('vue-loader')
+const path = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const portfinder = require('portfinder');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const VueLoaderPlugin = require('vue-loader').VueLoaderPlugin;
 
-const HOST = process.env.HOST
-const PORT = process.env.PORT && Number(process.env.PORT)
+const baseWebpackConfig = require('./webpack.base.conf');
+const config = require('../config');
+const utils = require('./utils');
+
+const HOST = process.env.HOST || config.dev.host;
+const PORT = process.env.PORT || config.dev.port;
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   mode: 'development',
-  module: {
-    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
-  },
-  // Update this line to use a valid devtool option
   devtool: 'eval-cheap-module-source-map',
+  module: {
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true }),
+  },
   devServer: {
     client: {
       logging: 'info',
       overlay: config.dev.errorOverlay ? { warnings: false, errors: true } : false,
     },
     historyApiFallback: {
-      rewrites: [
-        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
-      ],
+      rewrites: [{ from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') }],
     },
     hot: true,
     static: {
@@ -36,47 +35,56 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       publicPath: config.dev.assetsPublicPath,
     },
     compress: true,
-    host: HOST || config.dev.host,
-    port: PORT || config.dev.port,
+    host: HOST,
+    port: PORT,
     open: config.dev.autoOpenBrowser,
     proxy: config.dev.proxyTable,
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': require('../config/dev.env')
+      'process.env.NODE_ENV': JSON.stringify('development'),
     }),
+
+    // Inject .env from root or config dir
+    new Dotenv({
+      path: path.resolve(__dirname, '../config/.env'), // correct path
+      safe: false,
+    }),
+
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
-      inject: true
+      inject: true,
     }),
+
     new CopyWebpackPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, '../static'),
           to: config.dev.assetsSubDirectory,
           globOptions: {
-            ignore: ['.*']
-          }
-        }
-      ]
+            ignore: ['.*'],
+          },
+        },
+      ],
     }),
-    new VueLoaderPlugin()
-  ]
-})
 
+    new VueLoaderPlugin(),
+  ],
+});
+
+// 👇 Wrap with portfinder if needed (for dynamic dev port)
 module.exports = new Promise((resolve, reject) => {
-  portfinder.basePort = process.env.PORT || config.dev.port
+  portfinder.basePort = PORT;
   portfinder.getPort((err, port) => {
     if (err) {
-      reject(err)
+      reject(err);
     } else {
-      process.env.PORT = port
-      devWebpackConfig.devServer.port = port
+      process.env.PORT = port;
+      devWebpackConfig.devServer.port = port;
 
-      console.log(`Your application is running here: http://${devWebpackConfig.devServer.host || 'localhost'}:${port}`)
-
-      resolve(devWebpackConfig)
+      console.log(`🚀 Dev server running: http://${HOST}:${port}`);
+      resolve(devWebpackConfig);
     }
-  })
-})
+  });
+});
