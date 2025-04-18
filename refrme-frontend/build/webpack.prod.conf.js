86425e30d63b32cbeaa364a7dc5ed3a93e43a69c
Piotr Słupski <piotroxp@gmail.com>
2025-04-18 15:54:41 +0200

    Prod env

--- refrme-frontend/build/webpack.prod.conf.js
+++ refrme-frontend/build/webpack.prod.conf.js
@@ -16,6 +16,16 @@ fs.writeFileSync(
     JSON.stringify(baseWebpackConfig.resolve?.alias, null, 2)
 );
 
+const { definitions } = new Dotenv({
+  path: path.resolve(__dirname, '.env'), // load this now instead of the ones in '.env'
+  safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
+  allowEmptyValues: false, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
+  systemvars: false, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+  silent: false, // hide any errors
+  defaults: false, // load '.env.defaults' as the default values if empty.
+  prefix: 'VUE_APP_', // Only include environment variables that start with VUE_APP_
+});
+
 const webpackConfig = merge(baseWebpackConfig, {
   mode: 'production',
   module: {
@@ -71,6 +81,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({ ...definitions }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
       __VUE_OPTIONS_API__: JSON.stringify(true),
